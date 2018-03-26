e137febe4ef8406b9b90b8e059c3c3acf5f3a9eb
freddypauls <paasan-fred@hotmail.com>
2018-03-26 10:36:04 +0200

    2

--- app/src/main/java/no/ntnu/fredrik/lab3/MainActivity.java
+++ app/src/main/java/no/ntnu/fredrik/lab3/MainActivity.java
@@ -2,116 +2,201 @@ package no.ntnu.fredrik.lab3;
 
 import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.drawable.ShapeDrawable;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener2;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
-import android.view.Window;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener2 {
 
-    private float xPos, xAccel, xVel = 0.0f;
-    private float yPos, yAccel, yVel = 0.0f;
-    private float xMax, yMax;
-    private Bitmap ball;
-    private SensorManager sensorManager;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private CanvasView canvas;
+    private int circleRadius = 30;
+    private float circleX;
+    private float circleY;
+    private Handler handler;
+    private float sensorX;
+    private float sensorY;
+    private long lastSensorUpdateTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set orientation to landscape
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        BallView ballView = new BallView(this);
-        setContentView(ballView);
+        setContentView(R.layout.activity_main);
 
-        Point size = new Point();
+        //Sound and haptic feedback
+        final Vibrator vibratorFeedback = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+
+        //Sensors
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        assert sensorManager != null;
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer,SensorManager.SENSOR_DELAY_FASTEST);
+
+        //Display
         Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
         display.getSize(size);
-        xMax = (float) size.x - 100;
-        yMax = (float) size.y - 100;
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //Gets screen size for height and width
+        final int screenWidth = size.x;
+        final int screenHeight = size.y;
+
+        //Modified screen sizes
+        final int screenSizeHeight = screenHeight - 300;
+        final int screenSizeWidth =  screenWidth - 100;
+
+        circleX = screenWidth / 2 - circleRadius;
+        circleY = screenHeight / 2 - circleRadius;
+
+        canvas = new CanvasView(this);
+        setContentView(canvas);
+
+        handler = new Handler(){
+
+            @Override
+            public void handleMessage(Message message){
+                canvas.invalidate();
+            }
+
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                //Draws on the x axis
+                //makes sure that it does not draw outside screen
+                if (circleX < screenSizeWidth && circleX > 100){
+                    circleX = circleX + sensorY * 6.0f;
+                }
+                else if (circleX < screenSizeWidth){
+                    circleX = circleX + 100 -(sensorX * 6.0f);
+                    assert vibratorFeedback != null;
+                    vibratorFeedback.vibrate(50);
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+
+                }
+                else if (circleX > 0){
+                    circleX = circleX - 100 -(sensorX * 6.0f);
+                    assert vibratorFeedback != null;
+                    vibratorFeedback.vibrate(50);
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+
+                // Draws on the Y axis
+                //makes sure that it does not draw outside screen
+                if (circleY < screenSizeHeight && circleY > 100){
+                    circleY = circleY + sensorX * 6.0f;
+                }
+                else if (circleY < screenSizeHeight){
+                    circleY = circleY + 100 -(sensorY * 6.0f);
+                    assert vibratorFeedback != null;
+                    vibratorFeedback.vibrate(50);
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+                else if (circleY > 0){
+                    circleY = circleY - 100 -(sensorY * 6.0f);
+                    assert vibratorFeedback != null;
+                    vibratorFeedback.vibrate(50);
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+
+                // prints to log for development use
+                System.out.println("x " + circleX);
+                System.out.println("y " + circleY);
+                System.out.println("heigth " + screenSizeHeight);
+                System.out.println("width "+ screenSizeWidth);
+
+                handler.sendEmptyMessage(0);
+            }
+        },0,100);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
-    protected void onStop() {
-        sensorManager.unregisterListener(this);
-        super.onStop();
-    }
-
-    @Override
-    public void onFlushCompleted(Sensor sensor) {
-
-    }
-
+    // Updating sensor
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            xAccel = sensorEvent.values[0];
-            yAccel = -sensorEvent.values[1];
-            updateBall();
-        }
-    }
+        Sensor mySensor = sensorEvent.sensor;
 
-    private void updateBall() {
-        float frameTime = 0.666f;
-        xVel += (xAccel * frameTime);
-        yVel += (yAccel * frameTime);
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
 
-        float xS = (xVel / 2) * frameTime;
-        float yS = (yVel / 2) * frameTime;
+            long currentTime = System.currentTimeMillis();
 
-        xPos -= xS;
-        yPos -= yS;
+            if ((currentTime - lastSensorUpdateTime) > 100){
+                lastSensorUpdateTime= currentTime;
 
-        if (xPos > xMax) {
-            xPos = xMax;
-        } else if (xPos < 0) {
-            xPos = 0;
-        }
-
-        if (yPos > yMax) {
-            yPos = yMax;
-        } else if (yPos < 0) {
-            yPos = 0;
+                sensorX= sensorEvent.values[0];
+                sensorY= sensorEvent.values[1];
+            }
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-
     }
 
-    private class BallView extends View {
+    // Draws in Canvas View
+    public class CanvasView extends View {
+        private Paint ball;
+        private Paint background;
 
-        public BallView(Context context) {
+        public CanvasView(Context context){
             super(context);
-            Bitmap ballSrc = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
-            final int dstWidth = 100;
-            final int dstHeight = 100;
-            ball = Bitmap.createScaledBitmap(ballSrc, dstWidth, dstHeight, true);
-        }
+            setFocusable(true);
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            canvas.drawBitmap(ball, xPos, yPos, null);
-            invalidate();
+            ball = new Paint();
+            background = new Paint();
+        }
+        //Sets circle and rectangle
+        public void onDraw(Canvas screen){
+            //Circle
+            ball.setStyle(Paint.Style.FILL);
+            ball.setColor(Color.RED);
+            ball.setAntiAlias(true);
+            ball.setTextSize(30f);
+
+            //Rectangle
+            background.setStyle(Paint.Style.FILL);
+            background.setColor(Color.GRAY);
+            background.setAntiAlias(true);
+            background.setTextSize(30f);
+
+            //Rectangle position
+            int canvasW = getWidth();
+            int canvasH = getHeight();
+            Point centerOfCanvas = new Point(canvasW / 2, canvasH / 2);
+            int rectW = canvasW -100;
+            int rectH = canvasH -100;
+            int left = centerOfCanvas.x - (rectW / 2);
+            int top = centerOfCanvas.y - (rectH / 2);
+            int right = centerOfCanvas.x + (rectW / 2);
+            int bottom = centerOfCanvas.y + (rectH / 2);
+            Rect rect = new Rect(left, top, right, bottom);
+
+            //draws
+            screen.drawRect(rect, background);
+            screen.drawCircle(circleX,circleY,circleRadius,ball);
+
         }
     }
 }
